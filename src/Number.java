3e33b4cbbeb7f2f268a7c3b09b897ada4af362f7
vanduc2514 <vanduc2514@gmail.com>
2020-03-19 23:56:04 +0700

    Refactor #2

--- src/Number.java
+++ src/Number.java
@@ -5,223 +5,99 @@ public class Number {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number: ");
         int number = scanner.nextInt();
-        String result = null;
-        int hundreds = 0;
-        int tens = 0;
-        int ones = 0;
+        String result = "";
+        int hundreds = number / 100;
+        int tens = (number % 100) / 10;
+        int ones = number % 10;
+        boolean isSpecialNum = false;
+        boolean isLowerThan1000 = false;
 
-        if (number >= 0) {
-            if (number <= 10) {
-                switch (number) {
-                    case 0:
-                        result = "zero";
-                        break;
-                    case 1:
-                        result = "one";
-                        break;
-                    case 2:
-                        result = "two";
-                        break;
-                    case 3:
-                        result = "three";
-                        break;
-                    case 4:
-                        result = "four";
-                        break;
-                    case 5:
-                        result = "five";
-                        break;
-                    case 6:
-                        result = "six";
-                        break;
-                    case 7:
-                        result = "seven";
-                        break;
-                    case 8:
-                        result = "eight";
-                        break;
-                    case 9:
-                        result = "nine";
-                        break;
-                    case 10:
-                        result = "ten";
-                        break;
-                    default:
-                        result = "out of ability";
+        if (number >= 10 && number < 20) {
+            isSpecialNum = true;
+        }
+
+        if (number < 1000) {
+            isLowerThan1000 = true;
+        }
+
+        if (isLowerThan1000) {
+            switch (hundreds) {
+                case 0:
+                    break;
+                case 1:
+                    result = "one";
+                    break;
+                case 2:
+                    result = "two";
+                    break;
+                case 3:
+                    result = "three";
+                    break;
+                case 4:
+                    result = "four";
+                    break;
+                case 5:
+                    result = "five";
+                    break;
+                case 6:
+                    result = "six";
+                    break;
+                case 7:
+                    result = "seven";
+                    break;
+                case 8:
+                    result = "eight";
+                    break;
+                case 9:
+                    result = "nine";
+                    break;
+                default:
+                    result = "out of ability";
+            }
+
+            if (hundreds != 0) {
+                result += " " + "hundred" + " " + "and" + " ";
+            }
+
+            switch (tens) {
+                case 0:
+                    break;
+                case 1:
+                    isSpecialNum = true;
+                    tens = number % 10;
+                    break;
+                case 2:
+                    result += "twenty";
+                    break;
+                case 3:
+                    result += "thirty";
+                    break;
+                case 4:
+                    result += "forty";
+                    break;
+                case 5:
+                    result += "fifty";
+                    break;
+                case 6:
+                    result += "sixty";
+                    break;
+                case 7:
+                    result += "seventy";
+                    break;
+                case 8:
+                    result += "eighty";
+                    break;
+                case 9:
+                    result += "ninety";
+                    break;
+                default:
+                    result = "out of ability";
+            }
+
+            if (!isSpecialNum) {
+                if (tens != 0) {
+                    result += " ";
                 }
-            } else if (number < 20) {
-                switch (number) {
-                    case 11:
-                        result = "eleven";
-                        break;
-                    case 12:
-                        result = "twelve";
-                        break;
-                    case 13:
-                        result = "thirteen";
-                        break;
-                    case 14:
-                        result = "fourteen";
-                        break;
-                    case 15:
-                        result = "fifteen";
-                        break;
-                    case 16:
-                        result = "sixteen";
-                        break;
-                    case 17:
-                        result = "seventeen";
-                        break;
-                    case 18:
-                        result = "eighteen";
-                        break;
-                    case 19:
-                        result = "nineteen";
-                        break;
-                    default:
-                        result = "out of ability";
-                }
-            } else if (number < 100) {
-                tens = number / 10;
-                ones = number % 10;
-                switch (tens) {
-                    case 2:
-                        result = "twenty";
-                        break;
-                    case 3:
-                        result = "thirty";
-                        break;
-                    case 4:
-                        result = "forty";
-                        break;
-                    case 5:
-                        result = "fifty";
-                        break;
-                    case 6:
-                        result = "sixty";
-                        break;
-                    case 7:
-                        result = "seventy";
-                        break;
-                    case 8:
-                        result = "eighty";
-                        break;
-                    case 9:
-                        result = "ninety";
-                        break;
-                    default:
-                        result = "out of ability";
-                }
-
-                result += " ";
-
-                switch (ones) {
-                    case 0:
-                        break;
-                    case 1:
-                        result += "one";
-                        break;
-                    case 2:
-                        result += "two";
-                        break;
-                    case 3:
-                        result += "three";
-                        break;
-                    case 4:
-                        result += "four";
-                        break;
-                    case 5:
-                        result += "five";
-                        break;
-                    case 6:
-                        result += "six";
-                        break;
-                    case 7:
-                        result += "seven";
-                        break;
-                    case 8:
-                        result += "eight";
-                        break;
-                    case 9:
-                        result += "nine";
-                        break;
-                    default:
-                        result = "out of ability";
-                }
-            } else if (number < 1000) {
-                hundreds = number / 100;
-                tens = (number % 100) / 10;
-                ones = (number % 100) % 10;
-                switch (hundreds) {
-                    case 1:
-                        result = "one";
-                        break;
-                    case 2:
-                        result = "two";
-                        break;
-                    case 3:
-                        result = "three";
-                        break;
-                    case 4:
-                        result = "four";
-                        break;
-                    case 5:
-                        result = "five";
-                        break;
-                    case 6:
-                        result = "six";
-                        break;
-                    case 7:
-                        result = "seven";
-                        break;
-                    case 8:
-                        result = "eight";
-                        break;
-                    case 9:
-                        result = "nine";
-                        break;
-                    default:
-                        result = "out of ability";
-                }
-
-                result +=" " + "hundred" + " " + "and" + " ";
-
-                switch (tens) {
-                    case 0:
-                        break;
-                    case 1:
-                        result += "eleven" + " " + "and" + " ";
-                        break;
-                    case 2:
-                        result += "twenty";
-                        break;
-                    case 3:
-                        result += "thirty";
-                        break;
-                    case 4:
-                        result += "forty";
-                        break;
-                    case 5:
-                        result += "fifty";
-                        break;
-                    case 6:
-                        result += "sixty";
-                        break;
-                    case 7:
-                        result += "seventy";
-                        break;
-                    case 8:
-                        result += "eighty";
-                        break;
-                    case 9:
-                        result += "ninety";
-                        break;
-                    default:
-                        result = "out of ability";
-                }
-
-                result += " ";
-
                 switch (ones) {
                     case 0:
                         break;
@@ -256,8 +132,43 @@ public class Number {
                         result = "out of ability";
                 }
             }
-        } else {
-            result = "out of ability";
+
+            if (isSpecialNum) {
+                switch (tens) {
+                    case 0:
+                        result += "ten";
+                        break;
+                    case 1:
+                        result += "eleven";
+                        break;
+                    case 2:
+                        result += "twelve";
+                        break;
+                    case 3:
+                        result += "thirteen";
+                        break;
+                    case 4:
+                        result += "fourteen";
+                        break;
+                    case 5:
+                        result += "fifteen";
+                        break;
+                    case 6:
+                        result += "sixteen";
+                        break;
+                    case 7:
+                        result += "seventeen";
+                        break;
+                    case 8:
+                        result += "eighteen";
+                        break;
+                    case 9:
+                        result += "nineteen";
+                        break;
+                    default:
+                        result = "out of ability";
+                }
+            }
         }
         System.out.printf("The number %d is read: %s", number, result);
     }
